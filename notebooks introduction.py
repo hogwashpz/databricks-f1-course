cb1c19e3f25eb46d5b6d6d60a7ff510fe11c8d1c
hogwashpz <hogwashpz>
2021-08-04 13:42:06 +0000

    added magic commands

--- notebooks introduction.py
+++ notebooks introduction.py
@@ -1,6 +1,18 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC ### Notebooks Introduction
+# MAGIC * UI Intro
+# MAGIC * Magic Commands
+
+# COMMAND ----------
+
 msg = "Hello"
 
 # COMMAND ----------
 
 print(msg)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT "Hello"
