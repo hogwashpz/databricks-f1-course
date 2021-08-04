9f88dc5fb6d59ce9cdf8bbce3e2d43b802c45883
hogwashpz <hogwashpz>
2021-08-04 14:02:19 +0000

    commands

--- notebooks introduction.py
+++ notebooks introduction.py
@@ -16,3 +16,27 @@ print(msg)
 
 # MAGIC %sql
 # MAGIC SELECT "Hello"
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC ls
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC ls dbfs:/databricks-datasets/COVID/USAFacts/
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC head dbfs:/databricks-datasets/COVID/USAFacts/covid_confirmed_usafacts.csv
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC ps
+
+# COMMAND ----------
+
+
